0f4a54ba5e5621916ccd349c0f00c17c4e75a1fc
Scott Hoyt <scott@wildonion.io>
2016-02-10 22:33:17 -0800

    Added CreditCardCell.

---
+++ CreditCardTable/CreditCardCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreditCardCell.swift
+//  CreditCardTable
+//
+//  Created by Scott Hoyt on 2/10/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+import UIKit
+
+class CreditCardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
