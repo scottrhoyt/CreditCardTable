fad4bdbcf66a68cd92302e9e220b6346b4984b4e
Scott Hoyt <scott@wildonion.io>
2016-02-10 23:01:46 -0800

    Add cc cell.

--- CreditCardTable/CreditCardTableViewController.swift
+++ CreditCardTable/CreditCardTableViewController.swift
@@ -12,24 +12,14 @@ class CreditCardTableViewController: UITableViewController {
         
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.registerClass(CreditCardCell.self, forCellReuseIdentifier: CreditCardCell.reuseId)
+        navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
