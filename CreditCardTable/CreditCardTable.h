29e941f13582184f4efff125336fe21bc72e10c7
Scott Hoyt <scott@wildonion.io>
2016-02-10 21:42:36 -0800

    Initial Commit

---
+++ CreditCardTable/CreditCardTable.h
@@ -0,0 +1,19 @@
+//
+//  CreditCardTable.h
+//  CreditCardTable
+//
+//  Created by Scott Hoyt on 2/10/16.
+//  Copyright © 2016 Scott Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CreditCardTable.
+FOUNDATION_EXPORT double CreditCardTableVersionNumber;
+
+//! Project version string for CreditCardTable.
+FOUNDATION_EXPORT const unsigned char CreditCardTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreditCardTable/PublicHeader.h>
+
+
